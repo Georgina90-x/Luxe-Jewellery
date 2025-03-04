880f6d5001b60de5ed8f92ed67101d1eab3830be
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-03 20:10:58 -0500

    added sizes to product model for rings, necklaces and bracelets

--- products/models.py
+++ products/models.py
@@ -5,21 +5,25 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
     def get_friendly_name(self):
         return self.friendly_name
-    
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)  # code for ring sizes
+    bracelet_sizes = models.BooleanField(default=False, null=True, blank=True)  # code for bracelet sizes
+    necklace_sizes = models.BooleanField(default=False, null=True, blank=True)  # code for necklace sizes
     price = models.DecimalField(max_digits=9, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -27,4 +31,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
